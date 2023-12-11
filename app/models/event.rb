4f4317454dd46e9edcbf43e60afcbbd74188c895
Benjamin Galland <galland.benjamin98@gmail.com>
2023-12-11 15:44:25 +0100

    crud suite

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,21 @@ class Event < ApplicationRecord
   has_one :chatroom
   has_many :participants
   has_many :tags, through: :event_tags
+
+  validates :name, :address, :start_date, :end_date, presence:true
+  validates :max_people, numericality: { only_integer: true }
+  validate :date_cannot_be_in_the_past, :end_date_cannot_be_before_start_date
+
+  def end_date_cannot_be_before_start_date
+    if start_date.present? && end_date.present? && end_date < start_date
+      errors.add(:end_date, "can't be anterior at start date!")
+    end
+  end
+
+  def date_cannot_be_in_the_past
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "can't be in the past!")
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Clear database"
+User.destroy_all
+Event.destroy_all
+
+puts "Create users ---------------"
+puts "Create tal user"
+tal = User.create(username: "tal", email: "tal@gmail.com", password: "azerty", age: 29, location: "Sanary-sur-mer", description: "Porteur du projet NearNow. Fan de snow et de basket.")
+
+puts "Create events --------------"
+event1 = Event.create(
+  name: "Soirée spéciale au Wagon",
+  start_date: DateTime.new(2023,12,12,18,15),
+  end_date: DateTime.new(2023,12,12,23,00),
+  address: "21 rue Haxo Marseille",
+  description: "Venez nombreux!",
+  public: true,
+  max_people: 0,
+  user_id: tal.id
+  )
+puts "#{event1.name} created!"
