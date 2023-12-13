0ee7a7173370502021314fefee71456e750ae033
Benjamin Galland <galland.benjamin98@gmail.com>
2023-12-13 12:32:01 +0100

    chat without action cable

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,19 @@
+class MessagesController < ApplicationController
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.chatroom = @chatroom
+    @message.participant = Participant.where(user_id: current_user.id, event_id: @chatroom.event.id)[0]
+    if @message.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      render "chatrooms/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
